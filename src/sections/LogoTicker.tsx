b2d2dda3797a510b3e46b5fda7f6182a50c2098f
sid-4998 <siddhantjha34@gmail.com>
2025-11-29 12:22:18 +0530

    Logo ticker section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,25 @@
+import AcmeLogo from '@/assets/logo-acme.png';
+import ApexLogo from '@/assets/logo-apex.png';
+import CelestialLogo from '@/assets/logo-celestial.png';
+import EchoLogo from '@/assets/logo-echo.png';
+import PulseLogo from '@/assets/logo-pulse.png';
+import QuantumLogo from '@/assets/logo-quantum.png';
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+  return <section className='py-20 md:py-24'>
+    <div className="container">
+      <div className='flex items-center gap-5'>
+        <div className='flex-1 md:flex-none'>
+          <h2>Trusted by top innovative teams</h2>
+        </div>
+        <div className='flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
+          <div className='flex flex-none gap-14'>
+            {[AcmeLogo, ApexLogo, CelestialLogo, EchoLogo, PulseLogo, QuantumLogo].map((logo) => (
+            <img src={logo.src} key={logo.src} className='h-6 w-auto'/>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 };
