6f9020283aad492a50d419b9ba9dfc99a1ec566c
sid-4998 <siddhantjha34@gmail.com>
2025-11-29 11:18:15 +0530

    Hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,32 +1,26 @@
 import LogoIcon from '@/assets/logo.svg';
 import MenuIcon from '@/assets/icon-menu.svg';
+import { Button } from '@/components/Button';
 
 export const Header = () => {
-  return <header className='py-4 border-b border-white/15 md:border-none'>
+  return <header className='py-4 border-b border-white/15 md:border-none sticky top-0 z-10'>
     <div className="container">
-      <div className='flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto'>
+      <div className='flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur'>
         <div>
           <div className='border h-10 w-10 border-white/15 rounded-lg inline-flex justify-center items-center'>
             <LogoIcon className='h-8 w-8'/>
           </div>
         </div>
         <div className='hidden md:block'>
-          <nav className='flex gap-8'>
-            <a href="#" className='text-sm text-white/70 hover:text-white transition'>Features</a>
-            <a href="#" className='text-sm text-white/70 hover:text-white transition'>Developers</a>
-            <a href="#" className='text-sm text-white/70 hover:text-white transition'>Pricing</a>
-            <a href="#" className='text-sm text-white/70 hover:text-white transition'>Changelog</a>
+          <nav className='flex gap-8 text-sm'>
+            <a href="#" className='text-white/70 hover:text-white transition'>Features</a>
+            <a href="#" className='text-white/70 hover:text-white transition'>Developers</a>
+            <a href="#" className='text-white/70 hover:text-white transition'>Pricing</a>
+            <a href="#" className='text-white/70 hover:text-white transition'>Changelog</a>
           </nav>
         </div>
         <div className='flex gap-4 items-center'>
-          <button className='relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a20ba] shadow-[0_0_12px_#8c45ff]'>
-            <div className='absolute inset-0'>
-              <div className='rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]'></div>
-              <div className='rounded-lg border border-white/40 absolute inset-0 [mask-image:linear-gradient(to_top,black,transparent)]'></div>
-              <div className='rounded-lg shadow-[0_0_10px_rgba(140,69,255,0.7)_inset] absolute inset-0'></div>
-            </div>
-            <span>Join waitlist</span>
-          </button>
+          <Button>Join waitlist</Button>
           <MenuIcon className='md:hidden'/>
         </div>
       </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,32 @@
+import { Button } from '@/components/Button';
+import starsBG from '@/assets/stars.png';
+
 export const Hero = () => {
-  return <section>Hero</section>;
+  return <section className='h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]' 
+  style={{
+    backgroundImage: `url(${starsBG.src})`
+  }}>
+    <div className='absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgba(140,69,255,0.5)_15%,rgba(14,0,36,0.5)_78%,transparent)]'></div>
+    <div className='absolute h-64 w-64 md:h-96 md:w-96 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,60))] shadow-[-20px_-20px_50px_rgba(255,255,255,0.5),-20px_-20px_80px_rgba(255,255,255,0.1),0_0_50px_rgb(140,69,255)]'></div>
+    <div className='absolute h-[344px] w-[344px] md:h-[580px] md:w-[580px] rounded-full border opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <div className='absolute h-2 w-2 md:h-4 md:w-4 bg-white left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full'></div>
+      <div className='absolute h-2 w-2 md:h-4 md:w-4 bg-white left-1/2 top-0 -translate-x-1/2 -translate-y-1/2 rounded-full'></div>
+      <div className='absolute h-5 w-5 md:h-10 md:w-10 border border-white left-full top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full inline-flex justify-center items-center'>
+        <div className='h-2 w-2 md:h-4 md:w-4 bg-white rounded-full'></div>
+      </div>
+    </div>
+    <div className='absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed'></div>
+    <div className='absolute h-[544px] w-[544px] md:h-[980px] md:w-[980px] rounded-full border border-white opacity-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <div className='absolute h-2 w-2 md:h-4 md:w-4 bg-white left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full'></div>
+      <div className='absolute h-2 w-2 md:h-4 md:w-4 bg-white left-full top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full'></div>
+    </div>
+    <div className="container relative mt-16">
+      <h1 className='text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgba(74,32,138,0.5))] text-transparent bg-clip-text text-center'>Seo AI</h1>
+      <p className='text-lg md:text-xl text-center text-white/70 mt-5 max-w-xl mx-auto'>Elevate your site&apos;s visibility effortlessly with AI,
+       where smart technology meets user-friendly SEO tools.</p>
+       <div className='mt-5 flex justify-center'>
+          <Button>Join waitlist</Button>
+       </div>
+    </div>
+  </section>;
 };
