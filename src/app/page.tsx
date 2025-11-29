b42bde5362ece927d13094a991eeeedb01320881
sid-4998 <siddhantjha34@gmail.com>
2025-11-29 23:20:32 +0530

    Footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Pricing from '@/sections/Pricing'
 import Features from '@/sections/Features';
 import Testimonials from '@/sections/Testimonials';
 import CallToAction from '@/sections/CallToAction';
+import Footer from '@/sections/Footer';
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Testimonials/>
       <CallToAction/>
       <Pricing/>
+      <Footer/>
     </>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,32 @@
-export const Footer = () => {
-  return <footer>Footer</footer>;
+import Image from "next/image"
+import LogoIcon from "@/assets/logo.svg";
+import Xsocial from '@/assets/social-x.svg';
+import YTsocial from '@/assets/social-youtube.svg';
+import Instasocial from '@/assets/social-instagram.svg';
+
+export default function Footer() {
+  return <footer className="py-5 border-t border-white/15">
+    <div className="container">
+      <div className="flex flex-col lg:flex-row items-center gap-8">
+        <div className="flex gap-2 items-center lg:flex-1">
+          <Image src={LogoIcon} className='h-6 w-6 bg-white rounded-full' alt="Logo"></Image>
+          <div>Seo AI</div>
+        </div>
+        <div className="py-5">
+          <nav className="flex flex-col gap-5 lg:flex-row lg:gap-7 lg:flex-1 lg:justify-center">
+            <a href="#features" className="text-white/70 hover:text-white text-xs md:text-sm transition">Features</a>
+            <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">Developers</a>
+            <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">Company</a>
+            <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">Blog</a>
+            <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">Changelog</a>
+          </nav>
+        </div>
+        <div className="flex gap-5 lg:flex-1 lg:justify-end">    
+          <Image src={Xsocial} className='h-8 w-8 bg-slate-300 border border-white/15 rounded-lg' alt="X Logo"></Image>
+          <Image src={YTsocial} className='h-8 w-8 bg-slate-300 border border-white/15 rounded-lg' alt="Youtube Logo"></Image>
+          <Image src={Instasocial} className='h-8 w-8 bg-slate-300 border border-white/15 rounded-lg' alt="Instagram Logo"></Image>
+        </div>
+      </div>
+    </div>
+  </footer>
 };
