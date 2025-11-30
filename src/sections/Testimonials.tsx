dd88b909b8db19aee2264e1534b994ce8ba5e318
sid-4998 <siddhantjha34@gmail.com>
2025-11-30 11:08:07 +0530

    Testimonial animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,9 +1,10 @@
+'use client'
 import Image from 'next/image';
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
-import { div } from "framer-motion/client";
+import { motion } from 'framer-motion';
 
 const testimonials = [
   {
@@ -38,9 +39,21 @@ export default function Testimonials() {
       <h2 className='text-5xl md:text-6xl text-center font-medium tracking-tighter'>Beyond Expectations</h2>
       <p className='text-white/70 text-lg md:text-xl max-w-sm mx-auto tracking-tight text-center mt-5'>Our AI SEO tools have transformed our clients&apos; strategies.</p>
     </div>
-    <div className='overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
-      <div className='flex gap-5'>
-        {testimonials.map((testimonial) => (
+    <div className='flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
+      <motion.div 
+      initial={{
+        translateX: '-50%'
+      }}
+      animate={{
+        translateX: '0'
+      }}
+      transition={{
+        duration: 30,
+        repeat: Infinity,
+        ease: 'linear',
+      }}
+      className='flex gap-5 flex-none pr-5'>
+        {[...testimonials].map((testimonial) => (
           <div key={testimonial.name} className='border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgba(140,69,255,0.3),black)] max-w-xs md:max-w-md flex-none'>
             <div className='text-lg tracking-tight md:text-2xl'>{testimonial.text}</div>
             <div className='flex items-center gap-3 mt-5'>
@@ -54,7 +67,21 @@ export default function Testimonials() {
             </div>
           </div>
         ))}
-      </div>
+        {[...testimonials].map((testimonial1) => (
+          <div key={testimonial1.name} className='border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgba(140,69,255,0.3),black)] max-w-xs md:max-w-md flex-none'>
+            <div className='text-lg tracking-tight md:text-2xl'>{testimonial1.text}</div>
+            <div className='flex items-center gap-3 mt-5'>
+              <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,255)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
+                <Image src={testimonial1.avatarImg} alt = {`Avatar for ${testimonial1.name}`} className="h-11 w-11 rounded-lg grayscale"/>
+              </div>
+              <div>
+                <div>{testimonial1.name}</div>
+                <div className='text-white/50 text-sm'>{testimonial1.title}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
     </div>
   </section>;
 };
