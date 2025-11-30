230ec982bfda188ca90856704c3169c81b5029c8
sid-4998 <siddhantjha34@gmail.com>
2025-11-30 08:35:09 +0530

    Animation for hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,25 +1,87 @@
+'use client';
+
 import { Button } from '@/components/Button';
 import starsBG from '@/assets/stars.png';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export default function Hero() {
-  return <section className='h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]' 
+  const sectionReference = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionReference,
+    offset: ['start end', 'end start'],
+  });
+
+  const backgroundPositionY = useTransform(scrollYProgress, [0,1], [-300,300]);
+  
+  return <motion.section 
+  ref = {sectionReference}
+  className='h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]' 
   style={{
-    backgroundImage: `url(${starsBG.src})`
-  }}>
+    backgroundImage: `url(${starsBG.src})`,
+    backgroundPositionY,
+  }}
+  animate={{
+    backgroundPositionX: starsBG.width,
+  }}
+  transition={{
+    repeat: Infinity,
+    ease: 'linear',
+    duration: 30,
+  }}
+  >
     <div className='absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgba(140,69,255,0.5)_15%,rgba(14,0,36,0.5)_78%,transparent)]'></div>
     <div className='absolute h-64 w-64 md:h-96 md:w-96 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,60))] shadow-[-20px_-20px_50px_rgba(255,255,255,0.5),-20px_-20px_80px_rgba(255,255,255,0.1),0_0_50px_rgb(140,69,255)]'></div>
-    <div className='absolute h-[344px] w-[344px] md:h-[580px] md:w-[580px] rounded-full border opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+    <motion.div style = {{
+      translateY: '-50%',
+      translateX: '-50%',
+    }}
+    animate = {{
+      rotate: '1turn',
+    }}
+    transition = {{
+      repeat: Infinity,
+      duration: 30,
+      ease: 'linear',
+    }} 
+    className='absolute h-[344px] w-[344px] md:h-[580px] md:w-[580px] rounded-full border opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
       <div className='absolute h-2 w-2 md:h-4 md:w-4 bg-white left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full'></div>
       <div className='absolute h-2 w-2 md:h-4 md:w-4 bg-white left-1/2 top-0 -translate-x-1/2 -translate-y-1/2 rounded-full'></div>
       <div className='absolute h-5 w-5 md:h-10 md:w-10 border border-white left-full top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full inline-flex justify-center items-center'>
         <div className='h-2 w-2 md:h-4 md:w-4 bg-white rounded-full'></div>
       </div>
-    </div>
-    <div className='absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed'></div>
-    <div className='absolute h-[544px] w-[544px] md:h-[980px] md:w-[980px] rounded-full border border-white opacity-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+    </motion.div>
+    <motion.div 
+    style={{
+      translateY: '-50%',
+      translateX: '-50%',
+    }}
+    animate={{
+      rotate: '-1turn',
+    }}
+    transition={{
+      repeat: Infinity,
+      duration: 60,
+      ease: 'linear',
+    }}
+    className='absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed'></motion.div>
+    <motion.div 
+    style={{
+      translateY: '-50%',
+      translateX: '-50%',
+    }}
+    animate={{
+      rotate: '-1turn',
+    }}
+    transition={{
+      repeat: Infinity,
+      duration: 90,
+      ease: 'linear',
+    }}
+    className='absolute h-[544px] w-[544px] md:h-[980px] md:w-[980px] rounded-full border border-white opacity-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
       <div className='absolute h-2 w-2 md:h-4 md:w-4 bg-white left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full'></div>
       <div className='absolute h-2 w-2 md:h-4 md:w-4 bg-white left-full top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full'></div>
-    </div>
+    </motion.div>
     <div className="container relative mt-16">
       <h1 className='text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgba(74,32,138,0.5))] text-transparent bg-clip-text text-center'>Seo AI</h1>
       <p className='text-lg md:text-xl text-center text-white/70 mt-5 max-w-xl mx-auto'>Elevate your site&apos;s visibility effortlessly with AI,
@@ -28,5 +90,5 @@ export default function Hero() {
           <Button>Join waitlist</Button>
        </div>
     </div>
-  </section>;
+  </motion.section>;
 };
