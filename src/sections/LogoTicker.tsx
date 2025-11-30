f5d88ae07b73817dbe5f552536a308f3fd425fff
sid-4998 <siddhantjha34@gmail.com>
2025-11-30 09:02:54 +0530

    Logo ticker animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,9 +1,11 @@
+'use client'
 import AcmeLogo from '@/assets/logo-acme.png';
 import ApexLogo from '@/assets/logo-apex.png';
 import CelestialLogo from '@/assets/logo-celestial.png';
 import EchoLogo from '@/assets/logo-echo.png';
 import PulseLogo from '@/assets/logo-pulse.png';
 import QuantumLogo from '@/assets/logo-quantum.png';
+import { motion } from 'framer-motion';
 
 export default function LogoTicker() {
   return <section className='py-20 md:py-24'>
@@ -12,12 +14,43 @@ export default function LogoTicker() {
         <div className='flex-1 md:flex-none'>
           <h2>Trusted by top innovative teams</h2>
         </div>
-        <div className='flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
-          <div className='flex flex-none gap-14'>
-            {[AcmeLogo, ApexLogo, CelestialLogo, EchoLogo, PulseLogo, QuantumLogo].map((logo) => (
-            <img src={logo.src} key={logo.src} className='h-6 w-auto'/>
-            ))}
-          </div>
+        <div className='flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
+          <motion.div 
+          initial={{
+            translateX: '-50%'
+          }}
+          animate={{
+            translateX: '0'
+          }}
+          transition={{
+            repeat: Infinity,
+            duration: 30,
+            ease: 'linear',
+          }}
+          className='flex flex-none gap-14 pr-14 -translate-x-1/2'>
+            {[AcmeLogo, 
+              ApexLogo, 
+              CelestialLogo, 
+              EchoLogo, 
+              PulseLogo, 
+              QuantumLogo].map((logo) => (
+              <img 
+                src={logo.src} 
+                key={logo.src} 
+                className='h-6 w-auto' alt='Logo'/>
+              ))}
+              {[AcmeLogo, 
+              ApexLogo, 
+              CelestialLogo, 
+              EchoLogo, 
+              PulseLogo, 
+              QuantumLogo].map((logo1) => (
+                <img
+                  src={logo1.src}
+                  key={logo1.src}
+                  className='h-6 w-auto' alt='Logo'/>
+              ))}
+          </motion.div>
         </div>
       </div>
     </div>
